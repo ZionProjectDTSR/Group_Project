0540c123d8255ec68a86a3b9656d0957f55d40c5
MODERN <MODERN@MSI>
2025-10-22 09:56:54 +0530

    Added Traversal.java by Member2

---
+++ src/Traversal/Traversal.java
@@ -0,0 +1,23 @@
+package Traversal;
+
+import Member1_Graph.Graph;
+import java.util.*;
+
+public class Traversal {
+    public static void depthFirstSearch(Graph graph, String start) {
+        Set<String> visited = new HashSet<>();
+        Stack<String> stack = new Stack<>();
+        stack.push(start);
+
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            if (!visited.contains(current)) {
+                System.out.println("Visited: " + current);
+                visited.add(current);
+                for (String neighbor : graph.getNeighbors(current)) {
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+}
